2d17e5278b610fc0423ab9c50c64dacb84cdb876
Maheshchoda <choda.maheshbabu@gmail.com>
2019-01-16 17:29:15 +0530

    Updates

---
+++ precache-manifest.5dc3dd27acaef72c45a7a7aa23f6b6dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eabd8cfa9f1c4def674",
+    "url": "/ReportListing/static/js/runtime~main.2eabd8cf.js"
+  },
+  {
+    "revision": "7659e77f9159a1c715c1",
+    "url": "/ReportListing/static/js/main.7659e77f.chunk.js"
+  },
+  {
+    "revision": "234ceb3c1d50c4bf901c",
+    "url": "/ReportListing/static/js/1.234ceb3c.chunk.js"
+  },
+  {
+    "revision": "7659e77f9159a1c715c1",
+    "url": "/ReportListing/static/css/main.0649fac9.chunk.css"
+  },
+  {
+    "revision": "18ff511caed5b70df759923f93dc53f8",
+    "url": "/ReportListing/index.html"
+  }
+];
